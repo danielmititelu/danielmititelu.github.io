c1cd0e83f593cf7f6ac01db7cd4c6b403cc6e061
Dumitru-Daniel Mititelu <mttl.daniel@gmail.com>
2022-01-29 12:21:51 +0200

    Add svelte app

---
+++ src/routes/index.svelte
@@ -0,0 +1,6 @@
+<section class="text-center w-full h-screen 
+    bg-gradient-to-r from-blue-300 via-white-300 to-orange-300">
+    <h1 class="pt-10 text-7xl">First page</h1>
+    <div class="font-serif pt-12 text-2xl">Subtitle!</div>
+</section>
+    
