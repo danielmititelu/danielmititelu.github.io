71c90be654d9732d42b072a8090e3d6ff615a5a4
Dumitru-Daniel Mititelu <mttl.daniel@gmail.com>
2022-05-15 16:02:16 +0300

    Try moving to adapter-auto

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,7 @@
 import { mdsvex } from "mdsvex";
 import mdsvexConfig from "./mdsvex.config.js";
 import preprocess from "svelte-preprocess";
-import adapter from "@sveltejs/adapter-static";
+import adapter from "@sveltejs/adapter-auto";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -18,9 +18,6 @@ const config = {
 
   kit: {
     adapter: adapter(),
-
-    // hydrate the <div id="svelte"> element in src/app.html
-    //target: "#svelte",
   },
 };
 
